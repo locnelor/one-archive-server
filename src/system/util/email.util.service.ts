25ce2b9a89d715e5c411af6e0c25fc2bce7452d5
locnelor <1750354949@qq.com>
2022-05-09 15:34:48 +0800

    .env

--- src/system/util/email.util.service.ts
+++ src/system/util/email.util.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import nodemailer from 'nodemailer'
+import * as nodemailer from 'nodemailer'
 
 @Injectable()
 export class EmailUtilService {
@@ -8,14 +8,14 @@ export class EmailUtilService {
         port: 465,//上文获取的port
         secure: true,//上文获取的secure
         auth: {
-            user: '1295786542@qq.com', // 发送方的邮箱，可以选择你自己的qq邮箱
-            pass: 'xoilbezkqandhhga' // 上文获取的stmp授权码
+            user: process.env.email, // 发送方的邮箱，可以选择你自己的qq邮箱
+            pass: process.env.pass // 上文获取的stmp授权码
         }
     });
     public sendEmail(to: string, subject: string, html: string) {
         return new Promise((resolve, rejects) => {
             this.nodeMail.sendMail({
-                from: "1295786542@qq.com",
+                from: process.env.email,
                 to,
                 subject,
                 html
--- src/system/util/util.module.ts
+++ src/system/util/util.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from "@nestjs/common";
 import { CryptoUtilService } from "./crypto.util.service";
+import { EmailUtilService } from "./email.util.service";
 import { PathUtilService } from "./path.util.service";
 import { StrUtilService } from "./str.util.service";
 
-const providers = [CryptoUtilService, StrUtilService, PathUtilService]
+const providers = [CryptoUtilService, StrUtilService, PathUtilService, EmailUtilService]
 @Global()
 @Module({
     providers,
